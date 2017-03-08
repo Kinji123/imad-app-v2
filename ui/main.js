d09613b9fbbf157cb8a9ac7a856568c0dbab6f52
Kinji123 <kinjalroy100@gmail.com>
2017-03-08 16:39:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,6 @@ button.onclick= function(){
 };
 
 //make a request
-request.open('GET',"http://kinji123.imad.hasura-app.io/counter", true);
+request.open('GET','http://kinji123.imad.hasura-app.io/counter', true);
 request.send(null);
 };
