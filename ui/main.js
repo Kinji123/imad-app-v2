13398fefea430eeafb47b837e871bbb401e8c3d9
Kinji123 <kinjalroy100@gmail.com>
2017-03-08 11:04:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the content of main-txt
 var element=document.getElementById('main-txt');
 element.innerHTML='Kinjal Roy';
+
+//move the img
+var img=document.getElementById('madi');
+img.onclick= function(){
+    img.style.marginLeft ='100px';
+};
