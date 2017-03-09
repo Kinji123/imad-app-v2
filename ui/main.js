f54f092d630103aabcf348f7739832552b3f3011
Kinji123 <kinjalroy100@gmail.com>
2017-03-09 14:43:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,18 +23,33 @@ request.open('GET','/counter', true);
 request.send(null);
 };
 //capture name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make a request to the server
-    
-    //capture a list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0; i< names.length; i++){
-        list+='<li>' + names[i] + '</li>';
-    }
+//create a request to counter endpoint
+  var request= new XMLHttpRequest();
+  
+  
+  //capture a response and store it in the variable
+  request.onreadystatechange= function(){
+    if(request.readystate === XMLHttpRequest.DONE){
+        if(request.status === 200){
+             //capture a list of names and render it as a list
+            var names=['name1','name2','name3','name4'];
+            var list='';
+            for(var i=0; i< names.length; i++){
+            list+='<li>' + names[i] + '</li>';
+        }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+        }
+    }
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+//make a request
+request.open('GET','/counter', true);
+request.send(null);
+};
+    
+   
