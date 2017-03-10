a4682a32c5f574f54a30ed1cf17ea5b8f00c66a9
Kinji123 <kinjalroy100@gmail.com>
2017-03-10 09:23:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,36 @@ request.send(null);
 //capture name
 
 var submit=document.getElementById('submit_btn');
+var submit=document.getElementById('submit_btn2');
+
+submit.onclick = function(){
+//create a request to counter endpoint
+  var request= new XMLHttpRequest();
+  
+  
+  //capture a response and store it in the variable
+  request.onreadystatechange= function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status === 200){
+             //capture a list of names and render it as a list
+            var names= request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0; i< comments.length; i++){
+            list+='<li>' + comments[i] + '</li>';
+        }
+        var ul=document.getElementById('commentlist');
+        ul.innerHTML=list;
+        }
+    }
+};
+var commentInput=document.getElementById('comment');
+var comment=commentInput.value;
+//make a request
+request.open('GET','http://kinji123.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+};
+
 submit.onclick = function(){
 //create a request to counter endpoint
   var request= new XMLHttpRequest();
