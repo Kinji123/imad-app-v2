76a754af3ceaf5aea64285e010be61a073381a4e
Kinji123 <kinjalroy100@gmail.com>
2017-03-13 12:11:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/test-db', function(req,res){
     //make a select request
     //return a response with the results
     pool.query('select * from test', function(err, result){
