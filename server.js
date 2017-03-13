2298287a0c861413ddddca2df9540fa41e3f5ac0
Kinji123 <kinjalroy100@gmail.com>
2017-03-13 12:06:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config ={
+  user: 'kinji123',
+  database: 'kinji123',
+  host: 'db.imad.hasura-app.io',
+  password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -78,6 +85,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/text-db',function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('select * from test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 var counter=0;
 app.get('/counter', function(req,res){
