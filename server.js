4c4439c99386bbeb488778e734cdd7701033c8d8
Kinji123 <kinjalroy100@gmail.com>
2017-03-18 10:27:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/submit-name',function (req,res){ // URL: /submit-name?name=xxxx
      res.send(JSON.stringify(comments));
  });
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
       if(err){
           res.status(500).send(err.toString());
