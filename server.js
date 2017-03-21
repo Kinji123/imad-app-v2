a31f7b387ac0b2fe32e83c8e236738880bbc817d
Kinji123 <kinjalroy100@gmail.com>
2017-03-22 04:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto=require('crypto');
+var body-Parser=require('body-parser');
 var config ={
   host: 'db.imad.hasura-app.io',
   user: 'kinji123',
@@ -11,6 +12,7 @@ var config ={
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(body-Parser.json());
 
 var articles = {
     'article-one': {
@@ -110,6 +112,18 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
 });
 
+app.get('/create-user',function(req,res){
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbSring], function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('username successfully created: ' +username); 
+        }
+    });
+});
 var counter=0;
 app.get('/counter', function(req,res){
    counter= counter + 1;
